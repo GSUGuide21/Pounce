e9e03c43b904fb27cd9eab2449fa8d07c055f1cc
GSUGuide21 <71561603+GSUGuide21@users.noreply.github.com>
2021-09-15 01:57:43 -0400

    Removing package.json

--- logic.py
+++ logic.py
@@ -7,10 +7,18 @@ import commands
 def respond(data):
 	content = data["text"]
 	name = data["name"]
+	
+	message = {
+		"content": content,
+		"user": name
+	}
 
 	command_prefixes = ["pounce\s+", "p!\s*", "$", "(?:He(?:llo|y)\s+Pounce\,\s+)"]
 	command_pattern = r"^(?:{})(.*)".format("|".join(command_prefixes))
 
+	print(command_pattern)
+	print(re.search(command_pattern, content))
+	
 	if re.search(command_pattern, content):
 		match = re.search(command_pattern, content)
 		target = match.group()
@@ -21,15 +29,12 @@ def respond(data):
 		args = parts[1:]
 
 		if commands[command_name]:
-			return dispatch_command(command_name, args, message = { 
-				"content": content,
-				"user": name
-			})
-		return dispatch_response(content)
+			return dispatch_command(command_name, args, message)
+		return dispatch_response(message)
 		
-	return content
+	return dispatch_response(message)
 
-def dispatch_command(command_name, args, message, user):
+def dispatch_command(command_name, args, message):
 	content = message["content"]
 	return content
 
